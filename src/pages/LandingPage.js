df81f33f8480dc07f7370297f49fbd3db782c727
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-05-28 14:58:58 +0500

    initial Commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../components/Header'
+import Story from '../components/Story'
+import Showroom from '../components/Showroom'
+import Services from '../components/Services'
+import Sustainability from '../components/Sustainability'
+import Contact from './contact/Contact'
+import Footer from '../components/Footer'
+import HeroComponent from '../components/HeroComponent'
+
+function LandingPage() {
+  return (
+    <>
+    <Header/>
+    <HeroComponent/>
+    <Story/>
+    <Services/>
+    <Showroom/>
+    <Sustainability/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default LandingPage
